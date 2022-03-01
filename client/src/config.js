7f3be40d69ed7c79d3ed8f306d0191d3acb58101
amitg <amitgodosi1@gmail.com>
2022-03-01 14:17:36 +0200

    commit

--- client/src/config.js
+++ client/src/config.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-// export const axiosInstance = axios.create({
-//   baseURL: "https://amitgodosi-chat.herokuapp.com/api/",
-// });
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://amitgodosi-chat.herokuapp.com/api",
 });
